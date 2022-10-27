70bf9182d36512e895e11d170de06ca10109e86f
siunixdev <abd.siunix@gmail.com>
2022-10-28 00:01:48 +0800

    update data

--- src/data.js
+++ src/data.js
@@ -16,6 +16,24 @@ export const datas = [
     dascription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum, ipsum et lobortis pulvinar, velit libero hendrerit mauris, eget auctor lacus tellus at metus. Quisque tincidunt laoreet neque, nec congue eros sagittis at. Pellentesque non nunc a quam posuere ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque et lacinia libero. In ut viverra turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vestibulum congue erat sit amet tristique.',
     logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c9/Google_logo_%282013-2015%29.svg/1200px-Google_logo_%282013-2015%29.svg.png',
     category: 'SAAS',
+  },
+  {
+    title: 'Engine Room',
+    dascription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum, ipsum et lobortis pulvinar, velit libero hendrerit mauris, eget auctor lacus tellus at metus. Quisque tincidunt laoreet neque, nec congue eros sagittis at. Pellentesque non nunc a quam posuere ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque et lacinia libero. In ut viverra turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vestibulum congue erat sit amet tristique.',
+    logo: 'https://images.glints.com/unsafe/glints-dashboard.s3.amazonaws.com/company-logo/a3497993fd91eeb8851b0be547578111.jpg',
+    category: 'SAAS',
+  },
+  {
+    title: 'Sanity',
+    dascription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum, ipsum et lobortis pulvinar, velit libero hendrerit mauris, eget auctor lacus tellus at metus. Quisque tincidunt laoreet neque, nec congue eros sagittis at. Pellentesque non nunc a quam posuere ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque et lacinia libero. In ut viverra turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vestibulum congue erat sit amet tristique.',
+    logo: 'https://assets.website-files.com/5c8bce998bdcd3b7371625fa/624376c93c16bcd235c4c07d_sanity-logo.png',
+    category: 'Headless',
+  },
+  {
+    title: 'Apple pay',
+    dascription: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum rutrum, ipsum et lobortis pulvinar, velit libero hendrerit mauris, eget auctor lacus tellus at metus. Quisque tincidunt laoreet neque, nec congue eros sagittis at. Pellentesque non nunc a quam posuere ornare. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque et lacinia libero. In ut viverra turpis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Vivamus vestibulum congue erat sit amet tristique.',
+    logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b0/Apple_Pay_logo.svg/1280px-Apple_Pay_logo.svg.png',
+    category: 'B2C',
   }
 ]
 
